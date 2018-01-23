1eedf4d5d5bb62e5f625d6895701532dd3f3ff15
Alex Şuhan <alex.suhan@gmail.com>
2018-01-23 00:04:46 -0800

    Enable sharding for (left-deep) outer joins

--- QueryEngine/JoinHashTable.cpp
+++ QueryEngine/JoinHashTable.cpp
@@ -130,7 +130,7 @@ std::mutex JoinHashTable::join_hash_table_cache_mutex_;
 size_t get_shard_count(const Analyzer::BinOper* join_condition,
                        const RelAlgExecutionUnit& ra_exe_unit,
                        const Executor* executor) {
-  if (executor->isOuterJoin() || executor->containsLeftDeepOuterJoin()) {
+  if (executor->isOuterJoin()) {
     return 0;
   }
   const Analyzer::ColumnVar* lhs_col{nullptr};
